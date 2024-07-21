2cbb82cd2fd509aec41bc8872dff15136cfdfbe9
adervish <adervishaj159@gmail.com>
2024-07-20 21:38:52 -0400

    added isLoggedIn to dependancy array of useEffect

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,8 @@
 const router = require("express").Router();
-const {
-  getCurrentUser,
-  updateUser,
-} = require("../controllers/users");
+const { getCurrentUser, updateUser } = require("../controllers/users");
 const auth = require("../middlewares/auth");
 
 router.get("/me", auth, getCurrentUser);
 router.patch("/me", auth, updateUser);
-
-// router.get("/", getUsers);
-// router.get("/:userId", getUserById);
-// router.post("/", createUser);
-
-// router.post("/", signin)
-// router.post("/", signup)
-// router.get("/", items);
 
 module.exports = router;
